f7eeaa20a412d90165adf4cf64fa50bb71a3e996
arily <arily@arily.moe>
2022-07-31 12:09:53 +0900

    update

--- app/guweb/run-on-start.sh
+++ app/guweb/run-on-start.sh
@@ -1,10 +1,11 @@
 cd /osu-server/guweb && git pull;
 
 
-if [ "$IS_DEV" == "true" ];
+if [ "$IS_DEV" == "true" ]
 then
-  if ["$RUN_CODE_SERVER" == "true" ]; then code-server --bind-addr 0.0.0.0:8080 --auth none;
-  else CMD tail -f /dev/null;
-  fi;
-else python3 main.py;
+  if ["$RUN_CODE_SERVER" == "true" ]
+  then code-server --bind-addr 0.0.0.0:8080 --auth none
+  else tail -f /dev/null
+  fi
+else python3 main.py
 fi
