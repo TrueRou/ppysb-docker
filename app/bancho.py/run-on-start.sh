910b78762584501416dd2e06fd2891064fb53ba5
arily <arily@arily.moe>
2022-08-03 01:20:39 +0900

    everyone will be happy

--- app/bancho.py/run-on-start.sh
+++ app/bancho.py/run-on-start.sh
@@ -2,6 +2,9 @@ cd /osu-server/bancho.py && git pull;
 
 if [ "$IS_DEV" == "true" ]
 then
+  echo "Running in dev mode, app is not running."
   tail -f /dev/null
-else python3 main.py
+else
+  echo "Running in production mode, app is running."
+  python3 main.py
 fi
