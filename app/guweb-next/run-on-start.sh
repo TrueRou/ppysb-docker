71a1ef4015d8703e9fc2d890c2875f8e517864a5
Ari Arimaniac <a@ri.mk>
2022-10-19 13:13:59 +0900

    build

--- app/guweb-next/run-on-start.sh
+++ app/guweb-next/run-on-start.sh
@@ -1,10 +1,12 @@
-cd /osu-server/guweb-next && git pull;
+cd /osu-server/guweb-next && git pull
 
-if [ "$IS_DEV" == "true" ]
-then
-  echo "App is running in dev mode."
-  npm run dev
+if [ "$IS_DEV" == "true" ]; then
+  echo "Running in dev mode. App is not running."
+  tail -f /dev/null
 else
-  echo "App is running in production preview mode"
+  echo "App is running in preview mode."
+  # generate prisma client
+  npm_config_yes=true npx prisma generate
+  npm run build
   npm run preview
 fi
