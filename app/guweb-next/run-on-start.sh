3d4edd50dce593d7e9f289d07fff80dfd3b579a7
Usagi no Niku <chenbohan911@163.com>
2022-10-19 11:17:41 +0800

    fix reparability patch

--- app/guweb-next/run-on-start.sh
+++ app/guweb-next/run-on-start.sh
@@ -2,9 +2,9 @@ cd /osu-server/guweb-next && git pull;
 
 if [ "$IS_DEV" == "true" ]
 then
-  echo "Running in dev mode, app is running in dev mode"
+  echo "App is running in dev mode."
   npm run dev
 else
-  echo "Running in production mode. app is running in production preview mode"
+  echo "App is running in production preview mode"
   npm run preview
 fi
