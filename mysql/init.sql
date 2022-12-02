6f01377c72903ea184df1dee5545207046b3fac2
undefined <i@undefined.moe>
2022-12-02 19:53:50 +0800

    update base image

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,7 +1,7 @@
 USE mysql;
 CREATE DATABASE IF NOT EXISTS banchopy;
 USE banchopy;
-source /mysql/base.sql
+source /base.sql
 USE mysql;
 UPDATE user SET host = '%' WHERE user = 'root';
 flush privileges;
