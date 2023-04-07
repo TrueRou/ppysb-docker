a6017e858d89c85719ef55472dedcb3bbaf5e6c9
arily <arily@arily.moe>
2023-04-07 12:27:12 +0900

    include guccho

---
+++ guccho/run-on-start.sh
@@ -0,0 +1,12 @@
+cd /osu-server/guccho && git pull
+
+if [ "$IS_DEV" == "true" ]; then
+  echo "Running in dev mode. App is not running."
+  tail -f /dev/null
+else
+  echo "App is running in preview mode."
+  # generate prisma client
+  npm_config_yes=true npx prisma generate
+  npm run build
+  npm run preview
+fi
