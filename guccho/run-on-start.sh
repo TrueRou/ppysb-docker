487f880660f57907ccf4e031b341ce9bdf853d13
Arimaniac <arily@arily.moe>
2023-06-30 21:04:09 +0900

    fix guccho build

--- guccho/run-on-start.sh
+++ guccho/run-on-start.sh
@@ -4,9 +4,8 @@ if [ "$DRY_RUN" == "true" ]; then
   echo "Running in dev mode. App is not running."
   tail -f /dev/null
 else
-  echo "App is running in preview mode."
-  # generate prisma client
-  npm_config_yes=true npx prisma generate
+  echo "App is running in prod mode w/ dev env files."
+  yarn build:gen
   npm run build
-  npm run preview
+  npm run start:dev:inspect
 fi
