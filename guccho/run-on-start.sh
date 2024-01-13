74e83d77c8f8913ebde728b2cddd2eeadabf60d7
Arimaniac <arily@arily.moe>
2024-01-13 15:49:37 +0900

    progress

--- guccho/run-on-start.sh
+++ guccho/run-on-start.sh
@@ -1,11 +1,9 @@
-cd /osu-server/guccho && git pull
+cd /guccho && git pull
 
 if [ "$DRY_RUN" == "true" ]; then
-  echo "Running in dev mode. App is not running."
-  tail -f /dev/null
+	echo "Running in dev mode. App is not running."
+	tail -f /dev/null
 else
-  echo "App is running in prod mode w/ dev env files."
-  yarn build:gen
-  npm run build
-  npm run start:dev:inspect
+	echo "App is running in prod mode w/ dev env files."
+	pnpm start:prod
 fi
