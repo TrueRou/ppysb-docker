898e0a3a30e873ab10abef193b197234d08ef480
Usagi no Niku <chenbohan911@163.com>
2024-05-07 23:00:35 +0800

    accelerate init progress

--- mysql/init/init.sql
+++ mysql/init/init.sql
@@ -1,7 +1,18 @@
 USE mysql;
 CREATE DATABASE IF NOT EXISTS banchopy;
+
+set innodb_lock_wait_timeout=100;
+set sql_log_bin=off;
+set autocommit=0;
+
 USE banchopy;
+start transaction;
 source /mysql/base.sql
+commit;
+
+set sql_log_bin=on;
+set autocommit=1;
+
 USE mysql;
 UPDATE user SET host = '%' WHERE user = 'root';
 flush privileges;
