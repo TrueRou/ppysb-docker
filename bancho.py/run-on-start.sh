0100c70f14d516621e06c5b26b5115982c7a6d43
Usagi no Niku <chenbohan911@163.com>
2024-12-15 14:45:21 +0800

    fix issues of guccho

--- bancho.py/run-on-start.sh
+++ bancho.py/run-on-start.sh
@@ -3,6 +3,7 @@ cd /osu-server/bancho.py && git pull
 if [ "$PROD_MODE" == "true" ] || [ "$PROD_MODE" == "True" ]; then
   echo "Running in production mode."
   DEVELOPER_MODE=False DEBUG=False python3 main.py
+  tail -f /dev/null
 else
   echo "Running in dev mode, app is not running."
   tail -f /dev/null
--- guccho/run-on-start.sh
+++ guccho/run-on-start.sh
@@ -3,6 +3,7 @@ cd /osu-server/Guccho && git pull
 if [ "$PROD_MODE" == "true" ] || [ "$PROD_MODE" == "True" ]; then
   echo "Running in production mode."
   pnpm build && pnpm start:prod
+  tail -f /dev/null
 else
   echo "Running in dev mode, app is not running."
   tail -f /dev/null
